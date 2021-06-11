acf24f0becc6b43df2f392ee545aa47e41c21a20
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-06-11 23:10:26 +0200

    create model classes and network

---
+++ weatherforecast/lib/network/network.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:weatherforecast/model/weather_forecast_model.dart';
+import 'package:weatherforecast/util/forecast_util.dart';
+import 'package:http/http.dart';
+
+class Network{
+
+  Future<WeatherForecastModel> getWeatherForecast({String cityName})async{
+
+    var url = 'http://api.openweathermap.org/data/2.5/forecast?q=$cityName&APPID=${Util.appId}';
+
+
+     final response = await get(Uri.parse(Uri.encodeFull(url)));
+
+     if(response.statusCode==200){
+       return WeatherForecastModel.fromJson(json.decode(response.body));
+     }else{
+       throw Exception('Error getting weathear forecast');
+     }
+    
+  }
+}
---
+++ weatherforecast/lib/util/forecast_util.dart
@@ -0,0 +1,4 @@
+class Util{
+
+  static String appId = '28fec65357bce9e97b99ccf07bf4f659';
+}
