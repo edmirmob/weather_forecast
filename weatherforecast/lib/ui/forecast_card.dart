75498258322fb3c68d54aa47063f5249aed8327e
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-08-05 23:25:55 +0200

    Setup day in bottom ScrollView

--- weatherforecast/lib/ui/forecast_card.dart
+++ weatherforecast/lib/ui/forecast_card.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:weatherforecast/model/weather_forecast_model.dart';
+import 'package:weatherforecast/util/forecast_util.dart';
 
-Widget forecastCard(AsyncSnapshot<WeatherForecastModel> snapshot, int index){
-  
-  }
+Widget forecastCard(AsyncSnapshot<WeatherForecastModel> snapshot, int index) {
+  var forecastList = snapshot.data.list;
+  var dayOfWeek = '';
+  DateTime date =
+      DateTime.fromMillisecondsSinceEpoch(forecastList[index].dt * 1000);
+  var fullDate = Util.getFormattedDate(date);
+  dayOfWeek = fullDate.split(',')[0];
+
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '$dayOfWeek',
+            style: TextStyle(fontSize: 18, color: Colors.black87),
+          ),
+        ],
+      )
+    ],
+  );
+}
--- weatherforecast/lib/util/forecast_util.dart
+++ weatherforecast/lib/util/forecast_util.dart
@@ -6,6 +6,7 @@ class Util{
 
   static String getFormattedDate(DateTime dateTime){
 
-    return DateFormat('EEE, MMM, d, y').format(dateTime);
+    return DateFormat('EEEE, MMM, d, y').format(dateTime);
   }
+
 }
