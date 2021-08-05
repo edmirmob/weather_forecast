7e8acb53bf343a8fa1f9064568d2c0ea6b843e24
Edmir Čolo <79378369+edmirmob@users.noreply.github.com>
2021-08-05 10:38:34 +0200

    Formatting and showing  date

--- weatherforecast/lib/ui/mid_view.dart
+++ weatherforecast/lib/ui/mid_view.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:weatherforecast/model/weather_forecast_model.dart';
+import 'package:weatherforecast/util/forecast_util.dart';
 
 Widget midView(AsyncSnapshot<WeatherForecastModel>snapshot){
   var forecastList = snapshot.data.list;
-
   var city = snapshot.data.city.name;
     var country = snapshot.data.city.country;
+    var formattedDate =DateTime.fromMillisecondsSinceEpoch(forecastList[0].dt * 1000);
         Container midView = Container(
           child: Padding(
             padding: const EdgeInsets.all(14.0),
@@ -16,7 +17,8 @@ Widget midView(AsyncSnapshot<WeatherForecastModel>snapshot){
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                   color: Colors.black87
-                ),)
+                ),),
+                Text('${Util.getFormattedDate(formattedDate)}')
       ],
     ),
           ),
--- weatherforecast/lib/util/forecast_util.dart
+++ weatherforecast/lib/util/forecast_util.dart
@@ -1,4 +1,11 @@
+import 'package:intl/intl.dart';
+
 class Util{
 
   static String appId = '28fec65357bce9e97b99ccf07bf4f659';
+
+  static String getFormattedDate(DateTime dateTime){
+
+    return DateFormat('EEE, MMM, d, y').format(dateTime);
+  }
 }
